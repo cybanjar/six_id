4e5be4a90f3119d43bf0bff4e75af5695a7b48f8
cybanjar <cybanjar@gmail.com>
2020-10-25 20:10:31 +0700

    first commit six id

---
+++ lib/shared/shared_value.dart
@@ -0,0 +1,6 @@
+part of 'shared.dart';
+
+String apiKey = '81f636407cb7c40a749b98e11df89a9f';
+String imageBaseURL = 'https://image.tmdb.org/t/p';
+
+PageEvent prevPageEvent;
---
+++ lib/ui/pages/account_confirmation_page.dart
@@ -0,0 +1,64 @@
+part of 'pages.dart';
+
+class AccountConfirmationPage extends StatefulWidget {
+  final RegistrationData registrationData;
+
+  AccountConfirmationPage(this.registrationData);
+
+  @override
+  _AccountConfirmationPageState createState() =>
+      _AccountConfirmationPageState();
+}
+
+class _AccountConfirmationPageState extends State<AccountConfirmationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        context
+            .bloc<PageBloc>()
+            .add(GoToPreferencePage(widget.registrationData));
+        return;
+      },
+      child: Scaffold(
+        body: Container(
+          color: Colors.white,
+          padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+          child: ListView(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 20, bottom: 90),
+                    height: 56,
+                    child: Stack(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: GestureDetector(
+                            onTap: () {
+                              context.bloc<PageBloc>().add(
+                                  GoToPreferencePage(widget.registrationData));
+                            },
+                            child: Icon(Icons.arrow_back, color: Colors.black),
+                          ),
+                        ),
+                        Center(
+                          child: Text(
+                            "Confirm",
+                            style: blackTextFont.copyWith(fontSize: 18),
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
